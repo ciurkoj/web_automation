99bd9b11521686771561300dc2d9471d75a05984
ciurkoj <jakub1798@gmail.com>
2020-06-18 14:03:35 +0100

    Added user login feature with user profile page.
    User can login now, display its profile page with default image

--- bottyfy/settings.py
+++ bottyfy/settings.py
@@ -142,3 +142,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+LOGIN_REDIRECT_URL = 'ikariam-home'
+LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
