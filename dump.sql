41d5d0e3983f46dbf3f536a47bdd9a09242964b3
ciurkoj <jakub1798@gmail.com>
2020-06-19 16:59:10 +0100

    Added Password Reset functionality and ProfilePage
    Profile page displays user's profile picture and basic information,
    enalbles user to change their credential such as password or email.
    
    Password reset lets user to generate a link to reset their password.
    Emails are being sent by gmail account.

--- dump.sql
+++ dump.sql
@@ -90,7 +90,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +99,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add game',1,'add_game'),(2,'Can change game',1,'change_game'),(3,'Can delete game',1,'delete_game'),(4,'Can view game',1,'view_game'),(5,'Can add log entry',2,'add_logentry'),(6,'Can change log entry',2,'change_logentry'),(7,'Can delete log entry',2,'delete_logentry'),(8,'Can view log entry',2,'view_logentry'),(9,'Can add permission',3,'add_permission'),(10,'Can change permission',3,'change_permission'),(11,'Can delete permission',3,'delete_permission'),(12,'Can view permission',3,'view_permission'),(13,'Can add group',4,'add_group'),(14,'Can change group',4,'change_group'),(15,'Can delete group',4,'delete_group'),(16,'Can view group',4,'view_group'),(17,'Can add user',5,'add_user'),(18,'Can change user',5,'change_user'),(19,'Can delete user',5,'delete_user'),(20,'Can view user',5,'view_user'),(21,'Can add content type',6,'add_contenttype'),(22,'Can change content type',6,'change_contenttype'),(23,'Can delete content type',6,'delete_contenttype'),(24,'Can view content type',6,'view_contenttype'),(25,'Can add session',7,'add_session'),(26,'Can change session',7,'change_session'),(27,'Can delete session',7,'delete_session'),(28,'Can view session',7,'view_session');
+INSERT INTO `auth_permission` VALUES (1,'Can add game',1,'add_game'),(2,'Can change game',1,'change_game'),(3,'Can delete game',1,'delete_game'),(4,'Can view game',1,'view_game'),(5,'Can add log entry',2,'add_logentry'),(6,'Can change log entry',2,'change_logentry'),(7,'Can delete log entry',2,'delete_logentry'),(8,'Can view log entry',2,'view_logentry'),(9,'Can add permission',3,'add_permission'),(10,'Can change permission',3,'change_permission'),(11,'Can delete permission',3,'delete_permission'),(12,'Can view permission',3,'view_permission'),(13,'Can add group',4,'add_group'),(14,'Can change group',4,'change_group'),(15,'Can delete group',4,'delete_group'),(16,'Can view group',4,'view_group'),(17,'Can add user',5,'add_user'),(18,'Can change user',5,'change_user'),(19,'Can delete user',5,'delete_user'),(20,'Can view user',5,'view_user'),(21,'Can add content type',6,'add_contenttype'),(22,'Can change content type',6,'change_contenttype'),(23,'Can delete content type',6,'delete_contenttype'),(24,'Can view content type',6,'view_contenttype'),(25,'Can add session',7,'add_session'),(26,'Can change session',7,'change_session'),(27,'Can delete session',7,'delete_session'),(28,'Can view session',7,'view_session'),(29,'Can add profile',8,'add_profile'),(30,'Can change profile',8,'change_profile'),(31,'Can delete profile',8,'delete_profile'),(32,'Can view profile',8,'view_profile');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +124,7 @@ CREATE TABLE `auth_user` (
   `date_joined` datetime(6) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +133,7 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$180000$KpsIVEOwWYuJ$todqd5Yq6jMThlFcMZrPJF64wgLPKYax7PoDD4EACXo=','2020-06-15 14:08:30.624390',1,'ciurkoj','','','ciurkoj@outlook.com',1,1,'2020-06-15 14:08:12.962380');
+INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$180000$KpsIVEOwWYuJ$todqd5Yq6jMThlFcMZrPJF64wgLPKYax7PoDD4EACXo=','2020-06-18 12:51:39.677119',1,'ciurkoj','','','ciurkoj@outlook.com',1,1,'2020-06-15 14:08:12.962380'),(2,'pbkdf2_sha256$180000$PksduDQnAzHg$ejpLyxMaoJ7gFooyBgg0U0a6FEGzAzRSmS0OpyViukQ=','2020-06-18 12:51:51.866268',0,'test','','','',0,1,'2020-06-18 12:38:05.435126'),(3,'pbkdf2_sha256$180000$YWptPWn4BqWo$hcJjNz0eR0PieP2r2RO1PMuRuyxkxUbOfWA03VsJZvA=','2020-06-18 13:01:16.615991',0,'newuser','','','newuser@company.com',0,1,'2020-06-18 13:01:05.023920');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -215,7 +215,7 @@ CREATE TABLE `django_admin_log` (
   CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
   CONSTRAINT `django_admin_log_chk_1` CHECK ((`action_flag` >= 0))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -224,6 +224,7 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
+INSERT INTO `django_admin_log` VALUES (1,'2020-06-18 12:31:40.051248','1','ciurkoj Profile',1,'[{\"added\": {}}]',8,1),(2,'2020-06-18 12:38:05.555399','2','test',1,'[{\"added\": {}}]',5,1),(3,'2020-06-18 12:38:08.434676','2','test Profile',1,'[{\"added\": {}}]',8,1),(4,'2020-06-18 12:43:02.363070','2','test Profile',3,'',8,1),(5,'2020-06-18 12:43:02.372364','1','ciurkoj Profile',3,'',8,1),(6,'2020-06-18 12:43:12.629324','3','ciurkoj Profile',1,'[{\"added\": {}}]',8,1),(7,'2020-06-18 12:43:42.844476','4','test Profile',1,'[{\"added\": {}}]',8,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -240,7 +241,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -249,7 +250,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (2,'admin','logentry'),(4,'auth','group'),(3,'auth','permission'),(5,'auth','user'),(6,'contenttypes','contenttype'),(1,'ikariam','game'),(7,'sessions','session');
+INSERT INTO `django_content_type` VALUES (2,'admin','logentry'),(4,'auth','group'),(3,'auth','permission'),(5,'auth','user'),(6,'contenttypes','contenttype'),(1,'ikariam','game'),(7,'sessions','session'),(8,'users','profile');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -266,7 +267,7 @@ CREATE TABLE `django_migrations` (
   `name` varchar(255) NOT NULL,
   `applied` datetime(6) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -275,7 +276,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2020-06-15 14:06:27.877186'),(2,'auth','0001_initial','2020-06-15 14:06:28.095449'),(3,'admin','0001_initial','2020-06-15 14:06:28.705030'),(4,'admin','0002_logentry_remove_auto_add','2020-06-15 14:06:28.863255'),(5,'admin','0003_logentry_add_action_flag_choices','2020-06-15 14:06:28.891313'),(6,'contenttypes','0002_remove_content_type_name','2020-06-15 14:06:29.008732'),(7,'auth','0002_alter_permission_name_max_length','2020-06-15 14:06:29.066304'),(8,'auth','0003_alter_user_email_max_length','2020-06-15 14:06:29.093859'),(9,'auth','0004_alter_user_username_opts','2020-06-15 14:06:29.104386'),(10,'auth','0005_alter_user_last_login_null','2020-06-15 14:06:29.157166'),(11,'auth','0006_require_contenttypes_0002','2020-06-15 14:06:29.161787'),(12,'auth','0007_alter_validators_add_error_messages','2020-06-15 14:06:29.173742'),(13,'auth','0008_alter_user_username_max_length','2020-06-15 14:06:29.233908'),(14,'auth','0009_alter_user_last_name_max_length','2020-06-15 14:06:29.292991'),(15,'auth','0010_alter_group_name_max_length','2020-06-15 14:06:29.315852'),(16,'auth','0011_update_proxy_permissions','2020-06-15 14:06:29.328796'),(17,'ikariam','0001_initial','2020-06-15 14:06:29.365509'),(18,'sessions','0001_initial','2020-06-15 14:06:29.433430');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2020-06-15 14:06:27.877186'),(2,'auth','0001_initial','2020-06-15 14:06:28.095449'),(3,'admin','0001_initial','2020-06-15 14:06:28.705030'),(4,'admin','0002_logentry_remove_auto_add','2020-06-15 14:06:28.863255'),(5,'admin','0003_logentry_add_action_flag_choices','2020-06-15 14:06:28.891313'),(6,'contenttypes','0002_remove_content_type_name','2020-06-15 14:06:29.008732'),(7,'auth','0002_alter_permission_name_max_length','2020-06-15 14:06:29.066304'),(8,'auth','0003_alter_user_email_max_length','2020-06-15 14:06:29.093859'),(9,'auth','0004_alter_user_username_opts','2020-06-15 14:06:29.104386'),(10,'auth','0005_alter_user_last_login_null','2020-06-15 14:06:29.157166'),(11,'auth','0006_require_contenttypes_0002','2020-06-15 14:06:29.161787'),(12,'auth','0007_alter_validators_add_error_messages','2020-06-15 14:06:29.173742'),(13,'auth','0008_alter_user_username_max_length','2020-06-15 14:06:29.233908'),(14,'auth','0009_alter_user_last_name_max_length','2020-06-15 14:06:29.292991'),(15,'auth','0010_alter_group_name_max_length','2020-06-15 14:06:29.315852'),(16,'auth','0011_update_proxy_permissions','2020-06-15 14:06:29.328796'),(17,'ikariam','0001_initial','2020-06-15 14:06:29.365509'),(18,'sessions','0001_initial','2020-06-15 14:06:29.433430'),(19,'users','0001_initial','2020-06-18 12:29:29.733221');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -301,7 +302,7 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('k5hbi1ypcz95ch1pu7qikmg9lx1j4p8q','ZGI3Y2Q5MGEzNTg5OGNkNGZjMzljZmUxZmExNDczN2IxNDYwMzc2Yzp7Il9hdXRoX3VzZXJfaWQiOiIxIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiJhY2FiMDk4NjU1YjgxMDEyMjMxN2Q5NTQzNDFjNzAxYzAxODUwZWRmIn0=','2020-06-29 14:08:30.628928');
+INSERT INTO `django_session` VALUES ('2l4yqvkr51oqamb4dmkqx0ga1vfv85yj','ZGI0YjUwZWNiMDg3ODU5ZGEwMmQ5YjNmNzA2NTkzMWFlYjY1MWMzYjp7Il9hdXRoX3VzZXJfaWQiOiIzIiwiX2F1dGhfdXNlcl9iYWNrZW5kIjoiZGphbmdvLmNvbnRyaWIuYXV0aC5iYWNrZW5kcy5Nb2RlbEJhY2tlbmQiLCJfYXV0aF91c2VyX2hhc2giOiI5ODcxNjdkZjI1ODFkNTQyMWRhMGNlNTc0MTdlYzc2ZTNlMmRkYTk2In0=','2020-07-02 13:01:16.621860');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -332,6 +333,33 @@ LOCK TABLES `ikariam_game` WRITE;
 /*!40000 ALTER TABLE `ikariam_game` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ikariam_game` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users_profile`
+--
+
+DROP TABLE IF EXISTS `users_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8 */;
+CREATE TABLE `users_profile` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(100) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  CONSTRAINT `users_profile_user_id_2112e78d_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_profile`
+--
+
+LOCK TABLES `users_profile` WRITE;
+/*!40000 ALTER TABLE `users_profile` DISABLE KEYS */;
+INSERT INTO `users_profile` VALUES (3,'profile_pics/pngguru.com.png',1),(4,'default.jpg',2),(5,'default.jpg',3);
+/*!40000 ALTER TABLE `users_profile` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -342,4 +370,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-15 16:01:59
+-- Dump completed on 2020-06-18 14:15:08
--- ikariam/models.py
+++ ikariam/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Game(models.Model):
@@ -11,3 +12,16 @@ class Game(models.Model):
 
     def __str__(self):
         return self.gamers_name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
